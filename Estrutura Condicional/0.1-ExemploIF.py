940239d174db58467d527d3e98c2862b42b9e6e2
gui <guilhermeangelozzi@gmail.com>
2026-09-01 02:12:43 -0300

    Enhance comments in age verification example
    Added comments to clarify age check logic.

--- Estrutura Condicional/0.1-ExemploIF.py
+++ Estrutura Condicional/0.1-ExemploIF.py
@@ -6,7 +6,10 @@ atual = int(input("Informe o ano atual: "))
 
 a1 = nascimento - nascimento
 
+# SE a idade for maior ou igual a 18:
 if a1 > 18:
     print("Você é maior de idade.")
+
+# SENÃO (se ele NÃO FOR maior ou igual a 18, ou seja, se for menor):
 else:
     print("Você é menor de idade.")
